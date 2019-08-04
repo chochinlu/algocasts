b73baf7d31e268d9e668e7fb6afb5d020eaf2ca6
Cho-Ching Lu <chochinlu@gmail.com>
2019-08-04 18:01:17 +0800

    better

--- anagram/index.js
+++ anagram/index.js
@@ -8,33 +8,17 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function charMap(str) {
-  str = str.toLowerCase().replace(/\W/g, '');
-
-  const map = {};
-
-  for (const c of str) {
-    map[c] = map[c] ? map[c] + 1 : 1;
-  }
-
-  return map;
+function cleanStr(str) {
+  return str
+    .toLowerCase()
+    .replace(/\W/g, '')
+    .split('')
+    .sort()
+    .join('');
 }
 
 function anagrams(stringA, stringB) {
-  const mapA = charMap(stringA);
-  const mapB = charMap(stringB);
-
-  if (Object.keys(mapA).length !== Object.keys(mapB).length) {
-    return false;
-  }
-
-  for (const k of Object.keys(mapA)) {
-    if (mapA[k] !== mapB[k]) {
-      return false;
-    }
-  }
-
-  return true;
+  return cleanStr(stringA) === cleanStr(stringB);
 }
 
 module.exports = anagrams;
