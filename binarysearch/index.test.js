43d378dacf2a5a07f6b120e85e51dd75aa89ac40
Cho-Ching Lu <chochinlu@gmail.com>
2019-08-20 09:49:54 +0800

    update test cases

--- binarysearch/index.test.js
+++ binarysearch/index.test.js
@@ -1,6 +1,23 @@
 const binarySearch = require('./index');
 
-const sortedData = [16, 21, 22, 39, 41, 47, 49, 52, 57, 60, 65, 71, 77, 90, 91];
+const sortedData = [
+  -2,
+  16,
+  21,
+  22,
+  39,
+  41,
+  47,
+  49,
+  52,
+  57,
+  60,
+  65,
+  71,
+  77,
+  90,
+  91
+];
 
 test('target 77 is true', () => {
   const target = 77;
@@ -14,5 +31,15 @@ test('target 2 is false', () => {
 
 test('target 78 is false', () => {
   const target = 78;
+  expect(binarySearch(sortedData, target)).not.toBeTruthy();
+});
+
+test('target -2 is true', () => {
+  const target = -2;
+  expect(binarySearch(sortedData, target)).toBeTruthy();
+});
+
+test('target -1 is true', () => {
+  const target = -1;
   expect(binarySearch(sortedData, target)).not.toBeTruthy();
 });
